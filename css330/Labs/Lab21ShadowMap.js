49b8323799f1bdb572121951a6bed839e9e8ef6c
Jordamae00 <112561294+Jordamae00@users.noreply.github.com>
2022-11-29 22:41:06 -0500

    Update Lab21ShadowMap.js

--- css330/Labs/Lab21ShadowMap.js
+++ css330/Labs/Lab21ShadowMap.js
@@ -1,7 +1,4 @@
-// shadow map example
 
-// two objects: a rotatable cube and triangle
-// point light source behind triangle
 
 "use strict";
 
@@ -10,7 +7,7 @@ var gl;
 
 window.onload = init;
 
-var numCubeVertices  = 36;
+var numPlaneVertices  = 6;
 var numTriangleVertices = 3;
 
 var triangleInstanceMatrix, cubeInstanceMatrix;
@@ -47,22 +44,18 @@ var colorsArray = [];
 
 var cubeVertices = [
     vec4(-0.5, -0.5, 0.5, 1.2),
-    vec4(-0.5, 0.5, 0.5, 1.2),
-    vec4(0.5, 0.5, 0.5, 1.2),
     vec4(0.5, -0.5, 0.5, 1.2),
     vec4(-0.5, -0.5, -0.5, 1.2),
-    vec4(-0.5, 0.5, -0.5, 1.2),
-    vec4(0.5, 0.5, -0.5, 1.2),
     vec4(0.5, -0.5, -0.5, 1.2)
 ];
 
 var cubeColors = [
-    vec4(1.0, 0.0, 0.0, 1.0),  // red
-    vec4(1.0, 1.0, 0.0, 1.0),  // yellow
-    vec4(0.0, 1.0, 0.0, 1.0),  // green
-    vec4(0.0, 0.0, 1.0, 1.0),  // blue
-    vec4(1.0, 0.0, 1.0, 1.0),  // magenta
-    vec4(0.0, 1.0, 1.0, 1.0)   // cyan
+    vec4(1.0, 0.0, 0.0, 1.0),  
+    vec4(1.0, 1.0, 0.0, 1.0),  
+    vec4(0.0, 1.0, 0.0, 1.0),  
+    vec4(0.0, 0.0, 1.0, 1.0),  
+    vec4(1.0, 0.0, 1.0, 1.0),  
+    vec4(0.0, 1.0, 1.0, 1.0)   
 ];
 
 var triangleVertices = [
@@ -92,14 +85,9 @@ function quad(a, b, c, d) {
      colorsArray.push(cubeColors[a]);
 }
 
-function colorCube()
+function colorPlane()
 {
-    quad(0, 1, 5, 4);
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(4, 5, 6, 7);
-    quad(5, 1, 2, 6);
+    quad(1, 0, 2, 3);
 }
 
 function triangle(a, b, c) {
@@ -145,7 +133,7 @@ function init() {
 
 // generate cube and triangle data
 
-    colorCube();
+    colorPlane();
     triangle(0, 1, 2);
 
 //  Load shaders and initialize attribute buffers
@@ -246,7 +234,7 @@ function render() {
     gl.uniformMatrix4fv( gl.getUniformLocation(program1,
             "uInstanceMatrix"), false, flatten(cubeInstanceMatrix) );
 
-    gl.drawArrays(gl.TRIANGLES, 0, numCubeVertices);
+    gl.drawArrays(gl.TRIANGLES, 0, numPlaneVertices);
 
 // don't rotate traingle and render it
 
@@ -255,7 +243,7 @@ function render() {
     gl.uniformMatrix4fv(gl.getUniformLocation(program1,
             "uInstanceMatrix"), false, flatten(triangleInstanceMatrix));
 
-    gl.drawArrays(gl.TRIANGLES, numCubeVertices, numTriangleVertices);
+    gl.drawArrays(gl.TRIANGLES, numPlaneVertices, numTriangleVertices);
 
 // release buffers
 
@@ -302,13 +290,13 @@ function render() {
 
     gl.uniformMatrix4fv(gl.getUniformLocation(program2,
             "uInstanceMatrix"), false, flatten(cubeInstanceMatrix));
-    gl.drawArrays( gl.TRIANGLES, 0, numCubeVertices);
+    gl.drawArrays( gl.TRIANGLES, 0, numPlaneVertices);
 
 
     gl.uniformMatrix4fv(gl.getUniformLocation(program2,
             "uInstanceMatrix"), false, flatten(triangleInstanceMatrix));
-    gl.drawArrays(gl.TRIANGLES, numCubeVertices, numTriangleVertices);
+    gl.drawArrays(gl.TRIANGLES, numPlaneVertices, numTriangleVertices);
 
-    requestAnimationFrame(render)
+    setInterval( requestAnimationFrame(render) , 500) ;
 
 }
