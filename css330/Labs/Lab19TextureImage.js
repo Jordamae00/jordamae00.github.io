979997c8047ba35cd8b2b3602aa2a29f61b08ec8
Jordamae00 <112561294+Jordamae00@users.noreply.github.com>
2022-11-29 22:30:19 -0500

    Update Lab19TextureImage.js

--- css330/Labs/Lab19TextureImage.js
+++ css330/Labs/Lab19TextureImage.js
@@ -1,7 +1,7 @@
 "use strict";
 var gl;
 var points;
-// isoceles triangles to form a tetrahedron
+
     points=[
     vec4(  0.00 ,  0.50 ,  0.00 , 1.0 ),
     vec4( -0.50 , -0.50 ,  0.50 , 1.0 ),
@@ -15,28 +15,7 @@ var texture;
 var program;
 
 var texSize = 64;
-/*
-// Create a checkerboard pattern using floats
-var image1 = new Array()
-    for (var i =0; i<texSize; i++)  image1[i] = new Array();
-    for (var i =0; i<texSize; i++)
-        for ( var j = 0; j < texSize; j++)
-           image1[i][j] = new Float32Array(4);
-    for (var i =0; i<texSize; i++) for (var j=0; j<texSize; j++) {
-        var c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0));
-        image1[i][j] = [c, c, c, 1];
-    }
-
-// Convert floats to ubytes for texture
-var image2 = new Uint8Array(4*texSize*texSize);
-    for (var i = 0; i < texSize; i++)
-        for (var j = 0; j < texSize; j++)
-           for(var k =0; k<4; k++)
-                image2[4*texSize*i+4*j+k] = 255*image1[i][j][k];
-*/
 var texCoordsArray = [];
-
-// perpendicular texture w/isoceles triangle
 var texCoord = [
     vec2(0.5, 0),
     vec2(0  , 1),
@@ -46,16 +25,25 @@ var texCoord = [
 var positionsArray = [];
 var colorsArray = [];
 var vertexColors = [
-    vec4(0.0, 0.0, 1.0, 1.0),  // blue
-    vec4(1.0, 0.0, 0.0, 1.0),  // red
-    vec4(1.0, 1.0, 0.0, 1.0),  // yellow
-    vec4(0.0, 1.0, 0.0, 1.0)  // green
+    vec4(0.0, 0.0, 1.0, 1.0),  
+    vec4(1.0, 0.0, 0.0, 1.0),  
+    vec4(1.0, 1.0, 0.0, 1.0),  
+    vec4(0.0, 1.0, 0.0, 1.0)  
 ];
 
 window.onload = init;
 
 function configureTexture( image ) {
+    texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB,
+         gl.RGB, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+                      gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
+    gl.uniform1i(gl.getUniformLocation(program, "uTextureMap"), 0);
 }
 
 function triangle (a,b,c,triNum)
@@ -137,7 +125,8 @@ function init()
     gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(texCoordLoc);
 
-
+    var image = document.getElementById("texImage");
+    configureTexture(image);
 
     thetaLoc = gl.getUniformLocation(program, "uTheta");
 
